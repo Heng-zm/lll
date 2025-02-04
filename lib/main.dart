e811fd9fcfa06ca38711f329da9da7f0c465e0a5
unknown <hengk7401@gmail.com>
2025-02-04 22:50:28 +0700

    l

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:typed_data'; // Add this import for Uint8List
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:image/image.dart' as img;
 
 void main() {
   runApp(MyApp());
@@ -9,38 +11,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WebViewPage(
-        url: 'https://heng-zm.github.io/tretr.github.io/oo.html', // URL to load
-      ),
+      debugShowCheckedModeBanner: false,
+      home: ScreenSharingApp(),
     );
   }
 }
 
-
-class WebViewPage extends StatefulWidget {
-  final String url;
-
-  WebViewPage({Key? key, required this.url}) : super(key: key);
-
+class ScreenSharingApp extends StatefulWidget {
   @override
-  _WebViewPageState createState() => _WebViewPageState();
+  _ScreenSharingAppState createState() => _ScreenSharingAppState();
 }
 
-class _WebViewPageState extends State<WebViewPage> {
-  late WebViewController _controller;
+class _ScreenSharingAppState extends State<ScreenSharingApp> {
+  late http.Client client;
+  late http.StreamedResponse streamedResponse;
+  late Image screenImage;
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    // Initialize WebView platform
-    WebViewPlatform.instance;
+    client = http.Client();
+    fetchStream();
+  }
+
+  Future<void> fetchStream() async {
+    // Connect to the FastAPI screen-sharing stream
+    final url = Uri.parse('http://127.0.0.1:8000/video'); // Your FastAPI endpoint
+    streamedResponse = await client.send(http.Request('GET', url));
+
+    // Listen to the stream for MJPEG frames
+    streamedResponse.stream.listen((List<int> data) {
+      setState(() {
+        // Decode MJPEG frames into images
+        final img.Image image = img.decodeImage(Uint8List.fromList(data))!;
+        screenImage = Image.memory(Uint8List.fromList(img.encodeJpg(image)));
+        isLoading = false;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebViewWidget(
-        controller: _controller,  // Set the WebViewController
+      appBar: AppBar(title: Text("Screen Sharing")),
+      body: Center(
+        child: isLoading
+            ? CircularProgressIndicator()
+            : screenImage,  // Display the captured screen
       ),
     );
   }
